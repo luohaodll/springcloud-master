5aac7f73a9e4a49b81a0bbbbc07b6ee3915625cc
luohao <luohaodll@163.com>
2019-06-11 10:12:12 +0800

    默认的

---
+++ code/leyou/ly-user/ly-user-service/src/main/java/com/leyou/user/web/UserController.java
@@ -0,0 +1,66 @@
+package com.leyou.user.web;
+
+import com.leyou.user.pojo.User;
+import com.leyou.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+/**
+ * @Author: cuzz
+ * @Date: 2018/11/16 18:06
+ * @Description:
+ */
+@RestController
+public class UserController {
+    @Autowired
+    UserService userService;
+
+    /**
+     * 校验数据是否可用
+     * @param data
+     * @param type
+     * @return
+     */
+    @GetMapping("check/{data}/{type}")
+    public ResponseEntity<Boolean> checkUserData(@PathVariable("data") String data, @PathVariable("type") Integer type) {
+        return ResponseEntity.ok(this.userService.checkData(data, type));
+    }
+
+    /**
+     * 发送手机验证码
+     * @param phone
+     * @return
+     */
+    @PostMapping("code")
+    public ResponseEntity<Void> sendVerifyCode(String phone) {
+        this.userService.sendVerifyCode(phone);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    /**
+     * 注册
+     * @param user
+     * @param code
+     * @return
+     */
+    @PostMapping("register")
+    public ResponseEntity<Void> register(@Valid User user, @RequestParam("code") String code) {
+        this.userService.register(user, code);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    /**
+     * 根据用户名和密码查询用户
+     * @param username
+     * @param password
+     * @return
+     */
+    @GetMapping("query")
+    public ResponseEntity<User> queryUser(@RequestParam("username") String username, @RequestParam("password") String password) {
+        return ResponseEntity.ok(userService.queryUser(username, password));
+    }
+}
